b50f9af18ee17340216aa356bbbbd082a7969fbe
Utkarsh Gupta <77887030+utkarsh3020@users.noreply.github.com>
2022-07-07 14:48:49 +0530

    Add files via upload

---
+++ Day4/Second/Mul_Div.js
@@ -0,0 +1,15 @@
+// For Multiplication
+function multiplyBy()
+{
+    num1 = document.getElementById("first").value;
+    num2 = document.getElementById("second").value;
+    document.getElementById("result").innerHTML = num1 * num2;
+}
+
+// For Divide
+function divideBy() 
+{ 
+    num1 = document.getElementById("first").value;
+    num2 = document.getElementById("second").value;
+    document.getElementById("result").innerHTML = num1 / num2;
+} 
